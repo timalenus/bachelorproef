f6ecb8834eef8b3f7289b62b4b02b03009c7ec51
Tim Alenus <tim@partago.be>
2019-06-29 12:01:39 +0200

    dart project moved to separate repo +

--- simulation-tool/bin/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:simulation_tool/simulation_tool.dart' as simulation_tool;
-
-main(List<String> arguments) {
-  print('Hello world:!');
-}
--- simulation-tool/test/simulation_tool_test.dart
+++
@@ -1,6 +0,0 @@
-import 'package:simulation_tool/simulation_tool.dart';
-import 'package:test/test.dart';
-
-void main() {
-  
-}
